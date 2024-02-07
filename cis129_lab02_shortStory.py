5df25b0e6a298d9bc62f0fb619143ca16e4b52b7
Anthony Newlin <89446542+anewlin12@users.noreply.github.com>
2024-02-06 21:14:29 -0700

    Update cis129_lab02_shortStory.py
    replaced    print('Hello World!')   with
    
    print('Once apon a time in a small town, there lived a old yet curious cat, named Whiskers.')
    print('One day Whiskers was out for his daily stroll and noticed a rusty metal figure on the ground,')
    print('as he aproched it he noticed it was an old key. He was unsure who or what it belonged to so he')
    print('continued on his way and had seen a newspaper on the ground that stated "LOST TRESURE".')
    print('Whiskers read the articale that stated a tresure chest had washed apon shore and was missing a key,')
    print('Wihskers then took his chance and had taken the old rust key and requested to try and open it.')
    print('Meow! It opened, he was then giving the contents inside which contained a gallon of milk.')
    print('The End.')
    
    
    Which results to a short story about Whiskers.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,13 @@
-print('Hello, World!')
+# Author : Anthony Newlin.
+# First Python Lab
+# This program will output a message by using print statements
+# Update includes, Print statement changed from 'Hello World' to a short story.
+
+print('Once apon a time in a small town, there lived a old yet curious cat, named Whiskers.')
+print('One day Whiskers was out for his daily stroll and noticed a rusty metal figure on the ground,')
+print('as he aproched it he noticed it was an old key. He was unsure who or what it belonged to so he')
+print('continued on his way and had seen a newspaper on the ground that stated "LOST TRESURE".')
+print('Whiskers read the articale that stated a tresure chest had washed apon shore and was missing a key,')
+print('Wihskers then took his chance and had taken the old rust key and requested to try and open it.')
+print('Meow! It opened, he was then giving the contents inside which contained a gallon of milk.')
+print('The End.')
